25785b96c78c65a35a88739f2af98c5de6287741
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2024-06-30 09:01:21 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # You can use the region of your preference
   region = "us-east-1"
-  access_key = "AKIAQ3EGRGPL6EW5LWXC"
-  secret_key = "dQQAyrI3uKY2h9M8eWQigjXduzOaclCLEoeho87e"
+  access_key = "your-access-key"
+  secret_key = "your-secret-key"
 }
